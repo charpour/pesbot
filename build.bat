27685a3117ceb21add6d6cb07657bc0d67bc1998
4a47 <noplundy@gmail.com>
2019-01-22 13:09:42 +0000

    added linking

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 pushd src
-i686-w64-mingw32-g++ -o3 pesbot.cpp -o ../pesbot -lgdi32 -static-libgcc -static-libstdc++
+i686-w64-mingw32-g++ -o3 pesbot.cpp -o ../pesbot -lgdi32 -static-libgcc -static-libstdc++ -static -lpthread
 popd
